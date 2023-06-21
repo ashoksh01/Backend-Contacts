52f02bebe6308c029d9579114ef4d0126ba0b86a
Ashok Shrestha <ashokshrestha1077@gmail.com>
2023-06-21 07:29:06 +0545

    backend arctic of user contact and book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,50 +1,60 @@
 const asyncHandler = require("express-async-handler");
-const Book = require("../models/bookModel")
-const fs = require("fs")
-const path = require('path')
+const Book = require("../models/bookModel");
+const fs = require("fs");
+const path = require("path");
 
-const addBook = asyncHandler(async(req,res) => {
-       try {
-        console.log(req?.file)
-        const bookData = new Book({
-          ...req.body,
-          image: req.file.image,
-        });
-        const createBook = await bookData.save();
-        console.log("object")
-        fs.unlink(req.file.path, (err)=>{
-          console.log(err)
-        })
-        res.status(201).send(createBook);
-       } catch (error) {
-        res.json(error.message)
-       }
-})
+const listBooks = asyncHandler(async (req, res) => {
+  const books = await Book.find({});
 
+  return res.send(books);
+});
 
-const editBook = asyncHandler(async(req,res) => {
-    const _id = req.params.id;
-    const updateBooks = await Book.findByIdAndUpdate(_id, {...req.body,  image: req.file.image,}, {
-        new: true,
+const addBook = asyncHandler(async (req, res) => {
+  try {
+    console.log(req?.file);
+    const bookData = new Book({
+      ...req.body,
+      // image: req.file.image,
     });
-    res.status(200).send("update");
-})
+    const createBook = await bookData.save();
+    // console.log("object");
+    // fs.unlink(req.file.path, (err) => {
+    //   console.log(err);
+    // });
+    res.status(201).send(createBook);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
 
-const detail = asyncHandler(async(req,res)=>{
-  const id = req.params.id;
-  const existingBook = await Book.findById(id)
-  console.log(existingBook)
-  return res.send(existingBook)
-})
-const deleteBooks = asyncHandler(async(req,res)=>{
+const editBook = asyncHandler(async (req, res) => {
   const _id = req.params.id;
-  const delete_ = await Book.findOneAndDelete({_id})
-  return res.send("Delete Book")
-})
+  const updateBooks = await Book.findByIdAndUpdate(
+    _id,
+    { ...req.body, image: req.file.image },
+    {
+      new: true,
+    }
+  );
+  res.status(200).send("update");
+});
+
+const detail = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const existingBook = await Book.findById(id);
+  console.log(existingBook);
+  return res.send(existingBook);
+});
+const deleteBooks = asyncHandler(async (req, res) => {
+  const _id = req.params.id;
+  const delete_ = await Book.findOneAndDelete({ _id });
+  return res.send("Delete Book");
+});
 
 module.exports = {
-    addBook,
-    editBook,
-    deleteBooks,
-    detail
-}
+  addBook,
+  editBook,
+  deleteBooks,
+  detail,
+  listBooks,
+};
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {addBook, deleteBooks, editBook, detail} = require("../controllers/bookController");
+const {
+  addBook,
+  deleteBooks,
+  editBook,
+  detail,
+  listBooks,
+} = require("../controllers/bookController");
 const upload = require("../config/multer/multer");
 
-router.route("/addBook").post(upload.single("image") ,addBook)
-router.route("/deleteBook/:id").delete(deleteBooks)
-router.route("/updateBook/:id").put(upload.single("image") ,editBook)
-router.route("/detail/:id").get(detail)
+router.route("/").get(listBooks);
+router.route("/addBook").post(upload.single("image"), addBook);
+router.route("/deleteBook/:id").delete(deleteBooks);
+router.route("/updateBook/:id").put(upload.single("image"), editBook);
+router.route("/detail/:id").get(detail);
 
 module.exports = router;
-
